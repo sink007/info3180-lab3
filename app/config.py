a3fa7ae5f8e61eca13a6a3cb624f248dc3652cd0
sink007 <lhines2000@gmail.com>
2025-03-01 23:28:24 -0500

    lab complete

--- app/config.py
+++ app/config.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 class Config(object):
     """Base Config Object"""
-    DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    DEBUG =True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -17,6 +19,20 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data,
+        sender=(form.name.data, form.email.data),
+        recipients= ["to@example.com"] )
+        msg.body = form.message.data
+        mail.send(msg)
+        flash('Message sent')
+        return redirect(url_for("home"))
+    
+    return render_template('contact.html',form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
